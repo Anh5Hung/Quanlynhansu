e6e0dba14e79bdb9582a5a3c8472ffca8cab7f32
Admin <Admin@VANHUNG>
2023-08-19 18:13:31 +0700

    Add project files.

---
+++ Article05/DAL/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Article05.DAL
+{
+    public class DBConnection
+    {
+        public DBConnection()
+        {
+
+        }
+        public SqlConnection CreateConnection()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=VANHUNG\SQLEXPRESS; Initial Catalog = sale1; User Id = sa; Password=sa";
+            return conn;
+        }
+    }
+}
---
+++ Article05/GUI/CustomerGUI.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Article05.GUI
+{
+    partial class CustomerGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomerGUI));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btNew = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btEdit = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.cbArea = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_area = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.id_area});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(770, 280);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(83, 306);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(217, 20);
+            this.tbId.TabIndex = 3;
+            this.tbId.TextChanged += new System.EventHandler(this.tbId_TextChanged);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(83, 345);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(217, 20);
+            this.tbName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Khu vực";
+            // 
+            // btNew
+            // 
+            this.btNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btNew.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.btNew.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btNew.Image = ((System.Drawing.Image)(resources.GetObject("btNew.Image")));
+            this.btNew.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btNew.Location = new System.Drawing.Point(23, 478);
+            this.btNew.Name = "btNew";
+            this.btNew.Size = new System.Drawing.Size(92, 38);
+            this.btNew.TabIndex = 6;
+            this.btNew.Text = "Thêm";
+            this.btNew.UseVisualStyleBackColor = true;
+            this.btNew.Click += new System.EventHandler(this.btNew_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.BackColor = System.Drawing.Color.Red;
+            this.btDelete.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btDelete.Location = new System.Drawing.Point(205, 478);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(95, 38);
+            this.btDelete.TabIndex = 7;
+            this.btDelete.Text = "Xóa";
+            this.btDelete.UseVisualStyleBackColor = false;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btEdit
+            // 
+            this.btEdit.BackColor = System.Drawing.Color.Cyan;
+            this.btEdit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEdit.Location = new System.Drawing.Point(380, 478);
+            this.btEdit.Name = "btEdit";
+            this.btEdit.Size = new System.Drawing.Size(104, 38);
+            this.btEdit.TabIndex = 8;
+            this.btEdit.Text = "Sửa";
+            this.btEdit.UseVisualStyleBackColor = false;
+            this.btEdit.Click += new System.EventHandler(this.btEdit_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExit.Location = new System.Drawing.Point(580, 478);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(100, 38);
+            this.btExit.TabIndex = 9;
+            this.btExit.Text = "Thoát";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // cbArea
+            // 
+            this.cbArea.FormattingEnabled = true;
+            this.cbArea.Location = new System.Drawing.Point(83, 380);
+            this.cbArea.Name = "cbArea";
+            this.cbArea.Size = new System.Drawing.Size(217, 21);
+            this.cbArea.TabIndex = 10;
+            this.cbArea.SelectedIndexChanged += new System.EventHandler(this.cbArea_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(443, 301);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 120);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tải ảnh lên";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(43, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // id
+            // 
+            this.id.FillWeight = 200F;
+            this.id.HeaderText = "Mã Cửa Hàng";
+            this.id.Name = "id";
+            this.id.Width = 200;
+            // 
+            // name
+            // 
+            this.name.FillWeight = 350F;
+            this.name.HeaderText = "Tên Cửa Hàng";
+            this.name.Name = "name";
+            this.name.Width = 350;
+            // 
+            // id_area
+            // 
+            this.id_area.FillWeight = 200F;
+            this.id_area.HeaderText = "Khu vực";
+            this.id_area.Name = "id_area";
+            this.id_area.Width = 200;
+            // 
+            // CustomerGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(809, 528);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cbArea);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btEdit);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btNew);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CustomerGUI";
+            this.Text = "CustomerGUI";
+            this.Load += new System.EventHandler(this.CustomerGUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btEdit;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.ComboBox cbArea;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btNew;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_area;
+    }
+}
